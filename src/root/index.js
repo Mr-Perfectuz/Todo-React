ae2f393cd3d3cfe5385be10273c52d87e9bfbc89
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2022-12-23 03:47:29 +0900

    update

--- src/root/index.js
+++ src/root/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { G17, G18 } from './G17';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 
 root.render(
-  <React.StrictMode>
-    <G17 />
-    <G18 />
-  </React.StrictMode>
+  <div>
+  </div>
 );
